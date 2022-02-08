153fb9f52cbca5257f2b7449f05aa90968a5824d
Mitchell-Johnson <mitchelljoho@gmail.com>
2022-02-08 12:54:14 -0800

    next two functions done

--- distribution_sampler.js
+++ distribution_sampler.js
@@ -2,16 +2,12 @@ function randomRange(min, max) {
     return Math.random() * (max - min) + min;
 }
 
-function one_s_radius() {
-    // uses rejection sampling to sample our custom distribution
-    const PDF_RANGE = 0.5413
-    const PDF_DOMAIN = 5
+function R10() {
+    const RANGE = 0.5413, DOMAIN = 5
     while (true) {
-        var x = randomRange(0.0, PDF_DOMAIN)
-        var u = randomRange(0.0, PDF_DOMAIN)
-        var r = 2.0 * Math.pow(1.0, (3.0 / 2.0)) * Math.pow(Math.E, -x)
-        PDF = Math.pow(x, 2.0) * Math.pow(r, 2.0)
-        if (u <= PDF) return Math.round(x * 10) / 10
+        var x = randomRange(0.0, DOMAIN), u = randomRange(0.0, RANGE)
+        var fx = Math.pow(x, 2) * Math.pow((2 * Math.pow(1, (3 / 2)) * Math.pow(Math.E, -x)), 2)
+        if (u <= fx) return x
     }
 }
 
@@ -19,7 +15,7 @@ function one_s_radius() {
 const N = 10000
 var res = {}
 for (let i = 0; i < N; i++) {
-    var tmp = one_s_radius()
+    var tmp = R00()
     if (tmp in res) res[tmp]++
     else res[tmp] = 1
 }
--- distro.py
+++ distro.py
@@ -1,19 +1,33 @@
 import math
 import numpy as np
-PDF_RANGE = 0.5413
-PDF_DOMAIN = 5
 
+def R10():
+    DOMAIN,RANGE = 7.69,0.54
+    while True:
+        x,u = np.random.uniform(0.0, DOMAIN),np.random.uniform(0.0,RANGE)
+        r10 = 2 * math.pow(1, (3/2)) * math.pow(math.e, -x)
+        p10 = math.pow(x,2) * math.pow(r10,2)
+        if u < p10: return round(x * 10) / 10
+def R20():
+    DOMAIN,RANGE = 19.5,0.191
+    while True:
+        x,u = np.random.uniform(0.0, DOMAIN),np.random.uniform(0.0,RANGE)
+        r20 = math.pow((1/2), (3/2)) * (2-x)  * math.pow(math.e, (-x/2))
+        p20 = math.pow(x,2) * math.pow(r20,2)
+        if u < p20: return round(x * 10) / 10
+def R21():
+    DOMAIN,RANGE = 18.37,0.1954
+    while True:
+        x,u = np.random.uniform(0.0, DOMAIN),np.random.uniform(0.0,RANGE)
+        r21 = (1/math.sqrt(3)) * math.pow((1/2), (3/2)) * x * math.pow(math.e, (-x/2))
+        p21 = math.pow(x,2) * math.pow(r21,2)
+        if u < p21: return round(x * 10) / 10
 res = {}
 n = 100000
 for i in range(n):
-    x = np.random.uniform(0.0, PDF_DOMAIN)
-    u = np.random.uniform(0.0, PDF_RANGE)
-    r = 2.0 * math.pow(1.0, (3.0 / 2.0)) * math.pow(math.e, -x)
-    PDF = math.pow(x, 2.0) * math.pow(r, 2.0)
-    if u < PDF: 
-        val = round(x * 10) / 10
-        if val in res: res[val] += 1
-        else: res[val] = 1
+    val = R21()
+    if val in res: res[val] += 1
+    else: res[val] = 1
 
 keys = list(res.keys())
 keys.sort()
