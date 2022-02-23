919dcd6d8e86e0ebefeb67520b2869bc7afa590a
Mitchell-Johnson <mitchelljoho@gmail.com>
2022-02-23 11:57:10 -0800

    test

--- README.md
+++ README.md
@@ -3,3 +3,10 @@ theta -> angle from positive **Z** axis towards positive **X** axis
 - theta range [0 - 180] degrees OR [0 - PI] radians
 phi -> angle from positive **X** axis towards positive **y** axis
 - phi range [0 - 360] degrees OR [0 - 2PI] radians
+
+# Orbitals
+**1S** -> (n = 1 l = 0 m = 0)
+**2S** -> (n = 2 l = 0 m = 0)
+**2Px** -> (n = 2 l = 1 m = 0)
+**2Pz** -> (n = 2 l = 1 m = 1)
+**2Py** -> (n = 2 l = 1 m = -1)
--- js/orbitals/p_orbitals.js
+++ js/orbitals/p_orbitals.js
@@ -18,14 +18,7 @@ function ORB_2Px() {
 function ORB_2Py() {
     var x = [], y = [], z = []
     for (let i = 0; i < GRAPH_RESOLUTION; i++) {
-        var point = randomPoint(true)
-        var point2 = randomPoint(true)
-        point[0] = R21(), point[1] = Y10()
-        point2[0] = R21(), point2[1] = -Y10()
-        point = PolarToCartesian(point)
-        point2 = PolarToCartesian(point2)
-        x.push(point[0]), y.push(point[1]), z.push(point[2])
-        x.push(point2[0]), y.push(point2[1]), z.push(point2[2])
+        break;
     }
     return [x, y, z]
 }
@@ -33,14 +26,7 @@ function ORB_2Py() {
 function ORB_2Pz() {
     var x = [], y = [], z = []
     for (let i = 0; i < GRAPH_RESOLUTION; i++) {
-        var point = randomPoint(true)
-        var point2 = randomPoint(true)
-        point[0] = R21(), point[1] = Y10()
-        point2[0] = R21(), point2[1] = -Y10()
-        point = PolarToCartesian(point)
-        point2 = PolarToCartesian(point2)
-        x.push(point[0]), y.push(point[1]), z.push(point[2])
-        x.push(point2[0]), y.push(point2[1]), z.push(point2[2])
+        break;
     }
     return [x, y, z]
 }
